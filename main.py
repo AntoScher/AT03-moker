58d7c6ca2eaf30afe808291a322610b81988a472
AntoScher <asc8er@gmail.com>
2025-01-31 14:48:39 +0300

    функция get_random_cat_image(cat_api_key)

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from dotenv import load_dotenv
 # Загрузка переменных окружения из файла .env
 load_dotenv()
 api_key = os.getenv('api_key')
+cat_api_key= os.getenv('cat_api_key')
+
 
 # Проверка, что переменные окружения корректно загружены
-if not api_key:
-    raise ValueError(" API_KEY должны быть заданы в файле .env")
+if not api_key or not cat_api_key:
+    raise ValueError("api_key & cat_api_key должны быть заданы в файле .env")
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -29,3 +31,37 @@ def get_github_user(username):
        return response.json()
    else:
        return None
+
+if __name__ == "__main__":
+   print(get_weather(api_key, 'London'))
+
+#THE_CAT_API_KEY
+def get_random_cat_image(cat_api_key):
+    """
+    Функция для получения случайного изображения кошки с использованием TheCatAPI.
+
+    :param cat_api_key: Ключ доступа к API TheCatAPI
+    :return: URL случайного изображения кошки
+    """
+    url = "https://api.thecatapi.com/v1/images/search"
+    headers = {
+        'x-api-key': cat_api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data[0]['url']
+    else:
+        print(f"Ошибка: {response.status_code}")
+        return None
+
+
+# Пример использования функции
+
+cat_image_url = get_random_cat_image(cat_api_key)
+if cat_image_url:
+    print(f"URL случайного изображения кошки: {cat_image_url}")
+else:
+    print("Не удалось получить изображение кошки")
